c8d12245326da83629ab30927a7739b4282955b9
Sławomir Kurczewski <kurczewski7@gmai.com>
2016-11-24 14:31:09 +0100

    Podłączone zmiana kolorów tekstu i tła

--- app/src/main/java/pl/edu/pwr/wiz/laboratorium1/SettingsActivity.java
+++ app/src/main/java/pl/edu/pwr/wiz/laboratorium1/SettingsActivity.java
@@ -1,17 +1,29 @@
 package pl.edu.pwr.wiz.laboratorium1;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.View;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import yuku.ambilwarna.AmbilWarnaDialog;
 
 public class SettingsActivity extends AppCompatActivity {
     public static final int CHANGE_SETTINGS = 0;
     private int textColor, backgroundColor;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,17 +33,20 @@ public class SettingsActivity extends AppCompatActivity {
         TypedValue typedValue = new TypedValue();
 
         // @TODO Pobierz aktualne kolory */
-        textColor=this.getIntent().getIntExtra("textColor",0);
-        backgroundColor=this.getIntent().getIntExtra("backgroundColor",0);
+        textColor = this.getIntent().getIntExtra("textColor", 0);
+        backgroundColor = this.getIntent().getIntExtra("backgroundColor", 0);
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     public void onSave(View view) {
         Intent result = new Intent();
 
         // @TODO Przekaż dane powrotne z nowymi kolorami
-        result.putExtra("textColor",textColor);
-        result.putExtra("backgroundColor",backgroundColor);
+        result.putExtra("textColor", textColor);
+        result.putExtra("backgroundColor", backgroundColor);
 
         result.putExtra("txt", "Ustawienia zapisane");
         setResult(Activity.RESULT_OK, result);
@@ -57,5 +72,53 @@ public class SettingsActivity extends AppCompatActivity {
 
     public void onBackgroundColor(View view) {
         // @TODO Wstaw okno dialogowe do wyboru koloru tla analogicznie do tego uzytego przy wyborze koloru tekstu
+        AmbilWarnaDialog dialog = new AmbilWarnaDialog(this, textColor, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                backgroundColor = color;
+            }
+
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
+                // wybrano rezygnację z zapisu koloru tła
+            }
+        });
+        dialog.show();
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Settings Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
